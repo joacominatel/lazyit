fba6977000a2724ba9caa264c79c1f6cbcdecd59
Joaquin Minatel <joacominatel@gmail.com>
2026-05-25 14:12:01 -0300

    chore: import AccessGrantsModule in users module

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AssetAssignmentsModule } from '../asset-assignments/asset-assignments.module';
+import { AccessGrantsModule } from '../access-grants/access-grants.module';
 
 @Module({
-  imports: [AssetAssignmentsModule],
+  imports: [AssetAssignmentsModule, AccessGrantsModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
