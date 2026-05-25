ec6715d786aceba601abc15fd85fc9f2aa3eb496
Joaquin Minatel <joacominatel@gmail.com>
2026-05-25 06:24:09 -0300

    updt: export asset-assignment schema from shared barrel

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,6 +2,7 @@
 // Organization: schemas/ (zod + inferred types), constants/, utils/ (pure fns).
 export * from "./constants/app";
 export * from "./schemas/asset";
+export * from "./schemas/asset-assignment";
 export * from "./schemas/asset-category";
 export * from "./schemas/asset-model";
 export * from "./schemas/location";
