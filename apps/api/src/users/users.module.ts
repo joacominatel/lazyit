5afbe052d92d7f118ebf19f86b427e97128f9ce2
Joaquin Minatel <joacominatel@gmail.com>
2026-05-25 06:24:17 -0300

    updt: import AssetAssignmentsModule in users module

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AssetAssignmentsModule } from '../asset-assignments/asset-assignments.module';
 
 @Module({
+  imports: [AssetAssignmentsModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
