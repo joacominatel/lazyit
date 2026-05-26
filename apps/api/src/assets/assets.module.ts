0d94baacc939f6c2377a4c7bf37fdd603aee3214
Joaquin Minatel <joacominatel@gmail.com>
2026-05-26 03:45:28 -0300

    chore: wire AssetHistoryModule into the assets module

--- apps/api/src/assets/assets.module.ts
+++ apps/api/src/assets/assets.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AssetsController } from './assets.controller';
 import { AssetsService } from './assets.service';
 import { AssetAssignmentsModule } from '../asset-assignments/asset-assignments.module';
+import { AssetHistoryModule } from '../asset-history/asset-history.module';
 
 @Module({
-  imports: [AssetAssignmentsModule],
+  imports: [AssetAssignmentsModule, AssetHistoryModule],
   controllers: [AssetsController],
   providers: [AssetsService],
   exports: [AssetsService],
