55c1a127e535af78d74200e7d477c2396e9a7e7f
Joaquin Minatel <joacominatel@gmail.com>
2026-05-26 03:45:28 -0300

    chore: wire AssetHistoryModule into the asset-assignments module

--- apps/api/src/asset-assignments/asset-assignments.module.ts
+++ apps/api/src/asset-assignments/asset-assignments.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AssetAssignmentsController } from './asset-assignments.controller';
 import { AssetAssignmentsService } from './asset-assignments.service';
+import { AssetHistoryModule } from '../asset-history/asset-history.module';
 
 @Module({
+  imports: [AssetHistoryModule],
   controllers: [AssetAssignmentsController],
   providers: [AssetAssignmentsService],
   // Exported so AssetsModule and UsersModule can expose nested /assignments endpoints.
